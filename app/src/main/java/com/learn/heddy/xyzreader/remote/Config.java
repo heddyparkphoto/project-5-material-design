c9cec246902206f286effb1db88186f825bc3a64
hyeryung park <hyeryungpark@hyeryungs-MacBook-Pro.local>
2017-03-14 21:27:59 -0400

    Update Remote Endpoint url
    Switch to the latest forum url because this one has improved Aspect ratios and all the thumbnails corrected.
    Also remove unused transition config file for final project wrapup.

--- app/src/main/java/com/learn/heddy/xyzreader/remote/Config.java
+++ app/src/main/java/com/learn/heddy/xyzreader/remote/Config.java
@@ -8,7 +8,7 @@ import java.net.URL;
 /*
  * This file is from the Udacity starter code and has minor updates.
  *
- * 1.  Switch to Student's forum provided url when the url in the starter code stopped working
+ * 1.  Switch to Student's forum-provided url when the url in the starter code stopped working
  * before I completed this project.
  *
  */
@@ -23,7 +23,7 @@ public class Config {
              * Commenting out the out-of-service url temporarily
                 url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
             */
-            url = new URL("https://nspf.github.io/XYZReader/data.json" );
+            url = new URL("https://gist.githubusercontent.com/AtomicSpider/12b731e3e2a41f336cc1bd7fc904efbb/raw/3e6da3480b132f6d0eb99ca3711da4a2c901627e/data.json");
         } catch (MalformedURLException e) {
             Log.e(LOG_TAG, ""+ e);
         }
